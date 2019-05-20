368340656ef3fbba8e3f7c7dcdc76f6a9fb72080
Joe Pell <jpell3130@gmail.com>
2019-05-19 22:18:55 -0400

    added helper fuction to seperate lyric lines and add note

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,12 @@
 module TracksHelper
+  include ERB::Util
+
+  def ugly_lyrics(lyrics)
+    new_lyrics = ""
+    lyrics.each_line do |line|
+      new_line = "&#9835; #{line}"
+      new_lyrics += new_line
+    end
+    "<pre>#{new_lyrics}</pre>".html_safe
+  end
 end
